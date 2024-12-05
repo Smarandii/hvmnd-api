5ffe73ca68d1e0f7e816e8babf003e076a7ae9d0
Smarandii <olegsmarandi@gmail.com>
2024-12-05 09:30:32 +0300

    update endpoint paths | v2.1.0

--- main.go
+++ main.go
@@ -10,19 +10,19 @@ import (
 func main() {
 	db.InitDB()
 	http.HandleFunc("GET /api/v1/ping", handlers.Ping)
-	http.HandleFunc("GET /api/v1/user", handlers.GetUsers)
-	http.HandleFunc("GET /api/v1/user/{id}", handlers.GetUsers)
-	http.HandleFunc("POST /api/v1/user", handlers.CreateOrUpdateUser)
+	http.HandleFunc("GET /api/v1/users", handlers.GetUsers)
+	http.HandleFunc("GET /api/v1/users/{id}", handlers.GetUsers)
+	http.HandleFunc("POST /api/v1/users", handlers.CreateOrUpdateUser)
 
-	http.HandleFunc("GET /api/v1/node", handlers.GetNodes)
-	http.HandleFunc("GET /api/v1/node/{id}", handlers.GetNodes)
-	http.HandleFunc("PATCH /api/v1/node", handlers.UpdateNode)
+	http.HandleFunc("GET /api/v1/nodes", handlers.GetNodes)
+	http.HandleFunc("GET /api/v1/nodes/{id}", handlers.GetNodes)
+	http.HandleFunc("PATCH /api/v1/nodes", handlers.UpdateNode)
 
-	http.HandleFunc("GET /api/v1/payment", handlers.GetPayments)
-	http.HandleFunc("GET /api/v1/payment/{id}", handlers.GetPayments)
-	http.HandleFunc("POST /api/v1/payment", handlers.CreatePaymentTicket)
-	http.HandleFunc("PATCH /api/v1/payment/complete/{id}", handlers.CompletePayment)
-	http.HandleFunc("PATCH /api/v1/payment/cancel/{id}", handlers.CancelPayment)
+	http.HandleFunc("GET /api/v1/payments", handlers.GetPayments)
+	http.HandleFunc("GET /api/v1/payments/{id}", handlers.GetPayments)
+	http.HandleFunc("POST /api/v1/payments", handlers.CreatePaymentTicket)
+	http.HandleFunc("PATCH /api/v1/payments/complete/{id}", handlers.CompletePayment)
+	http.HandleFunc("PATCH /api/v1/payments/cancel/{id}", handlers.CancelPayment)
 
 	log.Fatal(http.ListenAndServe(":9876", nil))
 }
