98648dd275c9d95ba4937ab4b84d89dc92f03d42
Smarandii <olegsmarandi@gmail.com>
2024-12-18 19:57:21 +0300

    hvmnd-api | fix bug on creating user with null total_spent or null balance | v4.0.1

--- handlers/telegram_users.go
+++ handlers/telegram_users.go
@@ -127,27 +127,36 @@ func CreateOrUpdateTelegramUser(w http.ResponseWriter, r *http.Request) {
 
 	query := `
 		INSERT INTO public.users (
-			telegram_id, 
-			total_spent, 
-			balance, 
-			first_name, 
-			last_name, 
-			username, 
+			telegram_id,
+			total_spent,
+			balance,
+			first_name,
+			last_name,
+			username,
 			language_code,
 			banned
 		)
-		VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
+		VALUES (
+			$1,
+			COALESCE($2, 0), -- Default to 0 if null
+			COALESCE($3, 0), -- Default to 0 if null
+			$4,
+			$5,
+			$6,
+			$7,
+			$8
+		)
 		ON CONFLICT (telegram_id) DO UPDATE
-		SET 
-			total_spent = COALESCE(EXCLUDED.total_spent, public.users.total_spent), 
-			balance = COALESCE(EXCLUDED.balance, public.users.balance), 
-			first_name = COALESCE(EXCLUDED.first_name, public.users.first_name), 
-			last_name = COALESCE(EXCLUDED.last_name, public.users.last_name), 
-			username = COALESCE(EXCLUDED.username, public.users.username), 
+		SET
+			total_spent = COALESCE(EXCLUDED.total_spent, public.users.total_spent),
+			balance = COALESCE(EXCLUDED.balance, public.users.balance),
+			first_name = COALESCE(EXCLUDED.first_name, public.users.first_name),
+			last_name = COALESCE(EXCLUDED.last_name, public.users.last_name),
+			username = COALESCE(EXCLUDED.username, public.users.username),
 			language_code = COALESCE(EXCLUDED.language_code, public.users.language_code),
 			banned = COALESCE(EXCLUDED.banned, public.users.banned)
-		WHERE public.users.telegram_id = EXCLUDED.telegram_id
-		RETURNING id, telegram_id, total_spent, balance, first_name, last_name, username, language_code, banned
+		RETURNING
+			id, telegram_id, total_spent, balance, first_name, last_name, username, language_code, banned
 	`
 
 	var user models.User
